b4958ccf454b2858c9a667032844077493d229e9
kiyo-8jo <csta13054@gmail.com>
2025-09-13 15:02:42 +0900

    画面外観作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,29 @@
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-    </div>
+    <main className="h-screen w-full flex items-center justify-center text-center bg-orange-100 text-xl">
+      <div className="flex flex-col h-fit w-fit bg-gray-200  px-20 py-8 rounded-2xl">
+        <h2 className="text-4xl font-bold mb-15">Todos</h2>
+        <form className="flex flex-col mb-15">
+          <input
+            type="text"
+            className="bg-white rounded-2xl w-80 h-8 mb-15 px-5 border-none"
+          />
+          <button
+            type="submit"
+            className="bg-blue-400 w-80 h-8 cursor-pointer text-white rounded-2xl"
+          >
+            Add
+          </button>
+        </form>
+        <div>
+          <ul>
+            <li>・aaaaa</li>
+            <li>・aaaaa</li>
+            <li>・aaaaa</li>
+            <li>・aaaaa</li>
+          </ul>
+        </div>
+      </div>
+    </main>
   );
 }
